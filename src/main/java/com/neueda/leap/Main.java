d6b0cddc484b6bf8487b5507c427ddfaf18f0367
anikatay <anika.taylorcheng@gmail.com>
2026-08-24 20:18:17 +0000

    edited main.java

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,6 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: replace jeepers-leapers? with your team's actual name
-        System.out.println("Hello world to the firm's Sprint 1 project skeleton - we are the jeepers leapers (Group 2)");
+       System.out.println("my branch is the best");
     }
 }
