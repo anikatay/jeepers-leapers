29fa7e84fbffdaf3d2d7969d7c368d9c9cff8145
EC2 Default User <ec2-user@ip-10-14-141-36.eu-west-1.compute.internal>
2026-08-31 21:56:25 +0000

    Feature/setup:  Docker compose migration with Nginx setup

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -3,6 +3,22 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         // TODO: replace jeepers-leapers? with your team's actual name
-        System.out.println("This is Nadia changing the print statement)");
+        System.out.println("This is the Jeepers Leapers application!");
+        System.out.println("Container is up and running.");
+        System.out.println("Sleeping...");
+        System.out.println("Inspect container and check docker-compose networking...");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        while(true){
+            System.out.println("Periodic check...");
+            try {
+                Thread.sleep(10000); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
