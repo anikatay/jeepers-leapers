145137f29cd807b94c1f9f76a9df91909cf9f1dd
EC2 Default User <ec2-user@ip-10-14-142-215.eu-west-1.compute.internal>
2026-09-17 20:07:34 +0000

    updated getAccountsForUser in AccountController

--- src/main/java/com/neueda/leap/controller/AccountController.java
+++ src/main/java/com/neueda/leap/controller/AccountController.java
@@ -1,8 +1,8 @@
 package com.neueda.leap.controller;
 
 import com.neueda.leap.dto.AccountResponse;
-import com.neueda.leap.model.Account;
-import com.neueda.leap.repository.AccountRepository;
+import com.neueda.leap.service.AccountService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,29 +16,15 @@ import java.util.UUID;
 @RequestMapping("/api")
 public class AccountController {
 
-    private final AccountRepository accountRepository;
+    private final AccountService accountService;
 
-    public AccountController(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
     }
 
     @GetMapping("/accounts/{userId}")
     public ResponseEntity<List<AccountResponse>> getAccountsForUser(@PathVariable UUID userId) {
-        List<Account> accounts = accountRepository.findByUserUserId(userId);
-
-        if (accounts.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        List<AccountResponse> response = accounts.stream()
-                .map(a -> new AccountResponse(
-                        a.getAccountId(),
-                        a.getCurrency(),
-                        a.getBalance(),
-                        a.getStatus(),
-                        a.getCreatedAt()))
-                .toList();
-
-        return ResponseEntity.ok(response);
+        ResponseEntity<List<AccountResponse>> response = accountService.getAccountsForUser(userId);
+        return response;
     }
 }
