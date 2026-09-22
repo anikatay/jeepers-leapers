16309f477ca3cb9929b613925e6601132d7097ab
EC2 Default User <ec2-user@ip-10-14-142-215.eu-west-1.compute.internal>
2026-09-22 14:02:49 +0000

    Add exchange response DTO

---
+++ src/main/java/com/neueda/leap/dto/ExchangeResponse.java
@@ -0,0 +1,36 @@
+package com.neueda.leap.dto;
+
+public class ExchangeResponse {
+
+    private String exchangeId;
+    private String name;
+    private String region;
+    private String timezone;
+    private String currency;
+
+    public ExchangeResponse() {
+    }
+
+    public ExchangeResponse(String exchangeId, String name, String region, String timezone, String currency) {
+        this.exchangeId = exchangeId;
+        this.name = name;
+        this.region = region;
+        this.timezone = timezone;
+        this.currency = currency;
+    }
+
+    public String getExchangeId() { return exchangeId; }
+    public void setExchangeId(String exchangeId) { this.exchangeId = exchangeId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getRegion() { return region; }
+    public void setRegion(String region) { this.region = region; }
+
+    public String getTimezone() { return timezone; }
+    public void setTimezone(String timezone) { this.timezone = timezone; }
+
+    public String getCurrency() { return currency; }
+    public void setCurrency(String currency) { this.currency = currency; }
+}
