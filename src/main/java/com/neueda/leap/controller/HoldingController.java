fb68dbcc39486c102aa438eeac81b62c16965f5e
anikatay <anika.taylorcheng@gmail.com>
2026-09-23 21:28:21 +0000

    Stubbed out HoldingController API endpoints

--- src/main/java/com/neueda/leap/controller/HoldingController.java
+++ src/main/java/com/neueda/leap/controller/HoldingController.java
@@ -5,10 +5,14 @@ import com.neueda.leap.dto.HoldingResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,7 +25,7 @@ public class HoldingController {
         this.holdingService = holdingService;
     }
 
-    @GetMapping("/holding/{userId}")
+    @GetMapping("/holdings/{userId}")
     public ResponseEntity<HoldingResponse> getPortfolio(@PathVariable UUID userId) {
         HoldingResponse response = holdingService.getPortfolio(userId);
 
@@ -31,4 +35,26 @@ public class HoldingController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/holdings")
+    public ResponseEntity<List<HoldingResponse>> getAllHoldings() {
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/holdings/{accountId}")
+    public ResponseEntity<HoldingResponse> getHoldingsByAccount(@PathVariable UUID accountId) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/holdings")
+    public ResponseEntity<HoldingResponse> createHolding(@RequestBody HoldingResponse holdingResponse) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping("/{holdings/{accountId}/{instrumentId}/{quantity}")
+    public void updateHolding(@PathVariable UUID accountId, @PathVariable UUID instrumentId, @PathVariable int quantity) {
+        // TODO: Implement the logic to update the holding here
+    }
+
 }
