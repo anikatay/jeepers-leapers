53cb0b14766eea7f19993a3a1f0aad5311467d1d
shanyu-dabbiru <26.shanyu@gmail.com>
2026-09-23 14:41:30 +0000

    etl: add __init__.py for etl package initialization

---
+++ analytics/etl/__init__.py
@@ -0,0 +1,10 @@
+"""
+Analytics ETL Package
+
+Modules:
+- extract: Extract data from OLTP database
+- transform: Transform and aggregate data for OLAP
+- load: Load data into analytics schema
+- validate: Reconciliation and quality checks
+- config: Configuration and connection settings
+"""
