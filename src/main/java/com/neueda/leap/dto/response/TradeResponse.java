fda55396dce877f9f142506c5564bc9cd196462d
EC2 Default User <ec2-user@ip-10-14-142-215.eu-west-1.compute.internal>
2026-09-25 20:56:24 +0000

    Removed all setters fron TradeResponse

--- src/main/java/com/neueda/leap/dto/response/TradeResponse.java
+++ src/main/java/com/neueda/leap/dto/response/TradeResponse.java
@@ -10,7 +10,7 @@ public class TradeResponse {
     private String ticker;
     private String instrumentName;
     private String side;
-    private BigDecimal quantity;
+    private int quantity;
     private BigDecimal executionPrice;
     private BigDecimal tradeValue;
     private LocalDateTime executedAt;
@@ -19,7 +19,7 @@ public class TradeResponse {
     }
 
     public TradeResponse(UUID tradeId, String ticker, String instrumentName, String side,
-                         BigDecimal quantity, BigDecimal executionPrice, BigDecimal tradeValue,
+                         int quantity, BigDecimal executionPrice, BigDecimal tradeValue,
                          LocalDateTime executedAt) {
         this.tradeId = tradeId;
         this.ticker = ticker;
@@ -35,63 +35,35 @@ public class TradeResponse {
         return tradeId;
     }
 
-    public void setTradeId(UUID tradeId) {
-        this.tradeId = tradeId;
-    }
 
     public String getTicker() {
         return ticker;
     }
 
-    public void setTicker(String ticker) {
-        this.ticker = ticker;
-    }
 
     public String getInstrumentName() {
         return instrumentName;
     }
 
-    public void setInstrumentName(String instrumentName) {
-        this.instrumentName = instrumentName;
-    }
 
     public String getSide() {
         return side;
     }
 
-    public void setSide(String side) {
-        this.side = side;
-    }
-
-    public BigDecimal getQuantity() {
+    public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(BigDecimal quantity) {
-        this.quantity = quantity;
     }
 
     public BigDecimal getExecutionPrice() {
         return executionPrice;
     }
 
-    public void setExecutionPrice(BigDecimal executionPrice) {
-        this.executionPrice = executionPrice;
-    }
-
     public BigDecimal getTradeValue() {
         return tradeValue;
-    }
-
-    public void setTradeValue(BigDecimal tradeValue) {
-        this.tradeValue = tradeValue;
     }
 
     public LocalDateTime getExecutedAt() {
         return executedAt;
     }
 
-    public void setExecutedAt(LocalDateTime executedAt) {
-        this.executedAt = executedAt;
-    }
 }
